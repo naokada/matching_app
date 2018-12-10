17001b144a34978b3e754b4dbfbfc706926a4508
Nao Okada <naookada@Naos-MacBook-Pro.local>
2018-12-10 21:48:29 +0900

    make devise table

--- db/migrate/20181210120930_devise_create_users.rb
+++ db/migrate/20181210120930_devise_create_users.rb
@@ -6,6 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false, unique: true
+      t.string :nickname, null: false
+      t.string :gender
 
       ## Recoverable
       t.string   :reset_password_token
